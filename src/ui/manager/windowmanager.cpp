d117449bdb0adc9e2d988f027e5903615ca24cdf
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2024-07-16 18:42:40 +0500

    UI: Use MemPlumber to check for memory leaks

--- src/ui/manager/windowmanager.cpp
+++ src/ui/manager/windowmanager.cpp
@@ -26,6 +26,7 @@
 #include <form/tray/trayicon.h>
 #include <form/zone/zoneswindow.h>
 #include <fortsettings.h>
+#include <memplumber/memplumberutil.h>
 #include <stat/statmanager.h>
 #include <user/usersettings.h>
 #include <util/guiutil.h>
@@ -373,6 +374,8 @@ bool WindowManager::showProgramsWindow()
         setupProgramsWindow();
     }
 
+    MemPlumberUtil::start();
+
     showWindow(m_progWindow);
 
     return true;
@@ -382,6 +385,8 @@ void WindowManager::closeProgramsWindow()
 {
     if (closeWindow(m_progWindow)) {
         m_progWindow = nullptr;
+
+        MemPlumberUtil::checkAndStop();
     }
 }
 
